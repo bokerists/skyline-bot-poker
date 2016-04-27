d8e5129124810b2e3f39c6b5ba6f252f0314fc8d
dogwolf <ciccio20@tiscali.it>
2016-04-27 20:15:00 +0200

    Fourthù try

--- player.js
+++ player.js
@@ -8,12 +8,12 @@ exports = module.exports = {
     var rankcards;
     rankcards = 0;
 
-    for (var l=0;l< gamestate.cards.length;l++) {
-      if (gamestate.cards[l].rank ==  'J')  rankcards = rankcards + 10;
-      else if (gamestate.cards[l].rank == 'Q')  rankcards = rankcards +11;
-      else if (gamestate.cards[l].rank == 'K')  rankcards = rankcards +12;
-      else if (gamestate.cards[l].rank == 'A')  rankcards = rankcards +13;
-      else rankcards = rankcards + gamestate.cards[l].rank;
+    for (var l=0;l< gamestate.players[gamestate.me].cards.length;l++) {
+      if (gamestate.players[gamestate.me].cards[l].rank ==  'J')  rankcards = rankcards + 10;
+      else if (gamestate.players[gamestate.me].cards[l].rank == 'Q')  rankcards = rankcards +11;
+      else if (gamestate.players[gamestate.me].cards[l].rank == 'K')  rankcards = rankcards +12;
+      else if (gamestate.players[gamestate.me].cards[l].rank == 'A')  rankcards = rankcards +13;
+      else rankcards = rankcards + gamestate.players[gamestate.me].cards[l].rank;
     }
     var activeplayers;
     activeplayers = 0;
@@ -25,8 +25,8 @@ exports = module.exports = {
     if (rankcards <= 10) return 0;
     if (rankcards > 10) return gamestate.callAmount;
     //
-    // gamestate contains info about the state of the game.
-    // check the documentation at https://bot-poker.herokuapp.com/wiki#gamestate for further info about the data structure.
+    // gamestate contains info about the stagamestate.te .cardso the game.
+    // check contains info about the stagamestate.te .cards/bot-poker.herokuapp.com/wiki#gamestate for further contains info contains info about the stagamestate.te .cardsate.te .cardsructure.
 
     //
     // you just have to return the amount that you want to bet.
